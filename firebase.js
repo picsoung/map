52b0e79ed520eed5c25b9fece11d170fa203fb91
Nicolas Grenie <nicolas.grenie@gmail.com>
2012-03-03 18:26:52 -0800

    get city coords from Bing API

--- firebase.js
+++ firebase.js
@@ -68,7 +68,7 @@
 		});
 
 		$("#addPin").click(function() {
-			addPin(Math.random(), Math.random(), Math.random(), Math.random());
+			var city = searchCity($("#pinLookup").val());
 		});
 
 		$("#deletePin").click(function() {
@@ -104,15 +104,39 @@
 		bindUi();
 
 	}
+	
+	var searchCity = function (city_name){
+		$('#map_canvas').gmap('search', { 'address': city_name }, function(result, status) {
+		if ( status === 'OK' ) {
+			var city = new Object();
+			var r = result;
+			city.lat = r[0]['resources'][0]['point']['coordinates'][0];
+			city.long = r[0]['resources'][0]['point']['coordinates'][1];
+			city.name = r[0]['resources'][0]['address']['formattedAddress'];
+			city.country = r[0]['resources'][0]['address']['countryRegion'];
+			
+			if (city != false){
+				addPin(Math.random(), city.name, city.lat, city.long);
+			}else{
+				alert("could not find your pin");
+			}
+		}else{
+			return false;
+		}
+		
+	});
+	
+	}
 
 	var addPin = function(user, name, lat, lon) {
-
 		pinsPath.push({
 			user: username,
 			name: name,
 			lat: lat,
 			lon: lon
 		});
+		
+		
 	}
 
 	var pinReceived = function(id, data) {
